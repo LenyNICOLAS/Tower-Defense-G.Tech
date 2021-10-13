b70315ce76c30b510e611cf55d18bbdefa1687a2
Lény Nicolas <lnicolas@gaming.tech>
2021-10-13 14:52:03 +0200

    13/10/21 14:51
    Camera fini

---
+++ TD/Assets/Scripts/CameraPlacement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CameraPlacement : MonoBehaviour
+{
+
+	public float Speed = 30f;
+	public float BorderThickness = 10f;
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		
+
+		if (GameManager.GameIsOver)
+		{
+			this.enabled = false;
+			return;
+		}
+
+		if (Input.GetKey("z") || Input.GetKey("up") || Input.mousePosition.y >= Screen.height - BorderThickness)
+		{
+			transform.Translate(Vector3.forward * Speed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey("s") || Input.GetKey("down") || Input.mousePosition.y <= BorderThickness)
+		{
+			transform.Translate(Vector3.back * Speed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey("d") || Input.GetKey("right") || Input.mousePosition.x >= Screen.width - BorderThickness)
+		{
+			transform.Translate(Vector3.right * Speed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey("q") || Input.GetKey("left") || Input.mousePosition.x <= BorderThickness)
+		{
+			transform.Translate(Vector3.left * Speed * Time.deltaTime, Space.World);
+		}
+
+		
+
+	}
+}
--- TD/Assets/Scripts/GameManager.cs
+++ TD/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
    
     public static GameManager Instance { get; private set; }
+    public static bool GameIsOver { get; internal set; }
 
     public GameObject Canvas;
 
