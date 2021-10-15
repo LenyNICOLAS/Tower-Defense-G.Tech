01623a6942079c78976bd5aa48dd4a0f4ed22fb5
LenyNICOLAS <91902708+LenyNICOLAS@users.noreply.github.com>
2021-10-15 14:40:17 +0200

    15/10/21 14:37
    Deuxième type d'ennemis
    les tours ne volent plus, et ne tournent plus
    Le cristal seul tourne
    Ajoute décor arbres et herbe niveau 1

--- TD/Assets/Scripts/Spawn.cs
+++ TD/Assets/Scripts/Spawn.cs
@@ -8,6 +8,7 @@ public class Spawn : MonoBehaviour
 
     public Transform goal;
     public GameObject EnemyPrefab;
+    public GameObject SmallEnemyPrefab;
 
     public bool isLaunched = false;
 
@@ -17,12 +18,21 @@ public class Spawn : MonoBehaviour
 
         isLaunched = true;
 
-        for(int i=0; i<10; ++i)
+        for(int i=0; i<5; ++i)
         {
             GameObject enemy = Instantiate(EnemyPrefab, transform.position, Quaternion.identity) as GameObject;
 
             enemy.GetComponent<GoToGoal>().goal = goal;
             enemy.GetComponent<GoToGoal>().spawn = transform;
+
+        }
+
+        for (int i = 0; i < 5; ++i)
+        {
+            GameObject smallenemy = Instantiate(SmallEnemyPrefab, transform.position, Quaternion.identity) as GameObject;
+
+            smallenemy.GetComponent<GoToGoal>().goal = goal;
+            smallenemy.GetComponent<GoToGoal>().spawn = transform;
 
         }
     }
