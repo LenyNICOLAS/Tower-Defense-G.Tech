84a774986bc0f7bc0fa04f77f25f6d41f261b00b
ThomasChatrefou <91902256+ThomasChatrefou@users.noreply.github.com>
2021-10-15 18:50:44 +0200

    15/10 18:50
    Modification du menu

---
+++ TD/Assets/Scripts/Menu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public float speed;
+
+    private bool open, back;
+    private Vector3 a, b;
+
+
+    RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        open = false;
+        back = false;
+
+        a = transform.position;
+        b = new Vector3(-1280, 720, 0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+            rectTransform.position = Vector3.MoveTowards(rectTransform.position, b, Time.deltaTime * speed);
+        }
+
+        if (back)
+        {
+            rectTransform.position = Vector3.MoveTowards(rectTransform.position, a, Time.deltaTime * speed);
+        }
+    }
+
+    public void Open()
+    {
+        open = true;
+        back = false;
+    }
+
+    public void Back()
+    {
+        open = false;
+        back = true;
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Game Exit");
+        Application.Quit();
+    }
+}
