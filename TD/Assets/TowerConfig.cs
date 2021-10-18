8ae094715d8dc340b8b70f60b6d9a6a3abff4d1a
Lény Nicolas <lnicolas@gaming.tech>
2021-10-18 12:42:18 +0200

    18/10/21 12:42
    Deuxième tourelle + début UI Tower: Amélioration de destruction

---
+++ TD/Assets/TowerConfig.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+
+public class TowerConfig : MonoBehaviour
+{
+
+    public Canvas CanvasTower;
+
+
+    private Canvas Canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnMouseDown()
+    {
+
+
+
+
+        Canvas = Instantiate(CanvasTower) as Canvas;
+
+
+
+
+
+    }
+
+
+}
